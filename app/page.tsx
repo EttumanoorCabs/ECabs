0bc441d8c441534b48be19e5087962bca3eb353a
C S Vaishakh <vaishakhsubramonian@gmail.com>
2025-07-10 23:44:08 +0530

    intial commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Header from "@/components/header"
+import Lander from "@/components/landing"
+
+export default function Home() {
+  return (
+    <section>
+      <Header/>
+      <Lander/>
+    </section>
+  )
+}
