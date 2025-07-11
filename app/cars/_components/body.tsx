aa7eb83cf82cbeef938554a06e75699c36d6a33c
C S Vaishakh <vaishakhsubramonian@gmail.com>
2025-07-11 22:34:43 +0530

    feat :adedd the dynamic routing components

--- app/cars/_components/body.tsx
+++ app/cars/_components/body.tsx
@@ -1,17 +1,20 @@
 import React from "react";
 import CarCard from "./carCard";
 import CarsData from '../_data/cars.json';
+import Link from "next/link";
 
-export default function Cars () {
-    return(
+export default function Cars() {
+    return (
         <section className="py-12 px-4 bg-gray-50 min-h-screen">
             <div className="max-w-5xl mx-auto">
                 <div className="flex flex-wrap justify-center gap-8">
-                    {Object.values(CarsData).map((car, index) => (
-                        <CarCard key={index} {...car} />
+                    {Object.entries(CarsData).map(([carId, car], index) => (
+                        <Link key={index} href={`/cars/${carId}`}>
+                            <CarCard key={index} {...car} />
+                        </Link>
                     ))}
                 </div>
             </div>
         </section>
-    )
+    );
 }
