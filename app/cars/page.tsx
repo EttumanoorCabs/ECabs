c341ad75cca6c09b718137b5b84e53391b9473de
C S Vaishakh <vaishakhsubramonian@gmail.com>
2025-07-11 20:41:57 +0530

    feat : strucutred the fleet page

---
+++ app/cars/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import CarsGrid from "./_components/body";
+import Header from "./_components/header";
+
+export default function Fleet () {
+    return(
+        <section>
+            <Header/>
+            <CarsGrid/>
+        </section>
+    )
+}
