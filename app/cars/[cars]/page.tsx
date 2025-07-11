ce04ea607f5dabf1a0dd124d80d4c63bcb8199b7
C S Vaishakh <vaishakhsubramonian@gmail.com>
2025-07-11 22:39:09 +0530

    feat : structured the page

---
+++ app/cars/[cars]/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import CarData from "./_components/body";
+import Header from "./_components/header";
+
+export default function Car () {
+    return(
+        <section>
+            <Header/>
+            <CarData/>
+        </section>
+    )
+}
