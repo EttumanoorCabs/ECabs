3531305dccab6162fe7da549a73d76495a87a528
C S Vaishakh <vaishakhsubramonian@gmail.com>
2025-07-11 19:01:56 +0530

    feat : added the type Car

---
+++ types/types.ts
@@ -0,0 +1,9 @@
+export interface Car {
+    name: string
+    shortDescription: string
+    description: string
+    image: string
+    seats: number
+    fuelType: string
+    features: string[]
+}
