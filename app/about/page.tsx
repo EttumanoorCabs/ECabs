58d6a786d8b9fe20f116a99e035c5bd05d4f1ad1
C S Vaishakh <vaishakhsubramonian@gmail.com>
2025-07-11 13:59:58 +0530

    feat : strucutred the about page

---
+++ app/about/page.tsx
@@ -0,0 +1,12 @@
+import Header from "./_components/header";
+import Body from "./_components/body";
+
+
+export default function AboutUs() {
+    return (
+        <section>
+            <Header/>
+            <Body/>
+        </section>
+    );
+}
